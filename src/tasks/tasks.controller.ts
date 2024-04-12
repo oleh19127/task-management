43bafaeccf9317beb758ba6a662741376c06bf66
oleh19127 <oleh19127@gmail.com>
2024-04-12 14:39:50 +0300

    Defining a Task Model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import type { ITask } from './task.module';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): ITask[] {
     return this.tasksService.getAllTasks();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import type { ITask } from './task.module';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: ITask[] = [];
 
   getAllTasks() {
     return this.tasks;
